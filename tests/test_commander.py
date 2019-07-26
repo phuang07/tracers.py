2c3316ffdc751a8d9298b1d9f64321f454e10e2b
MarkKoz <KozlovMark@gmail.com>
2019-07-25 19:43:11 -0700

    Add Commander tests

---
+++ tests/test_commander.py
@@ -0,0 +1,53 @@
+import unittest
+
+from algorithm_visualizer import commander
+from algorithm_visualizer import Commander
+
+
+class CommanderTests(unittest.TestCase):
+    def test_commander_max_commands(self):
+        Commander.commands = [None for _ in range(commander._MAX_COMMANDS)]
+
+        with self.assertRaisesRegex(RuntimeError, "Too Many Commands"):
+            Commander()
+
+        Commander.commands = []
+
+    def test_commander_max_objects(self):
+        Commander._objectCount = 200
+
+        with self.assertRaisesRegex(RuntimeError, "Too Many Objects"):
+            Commander()
+
+        Commander._objectCount = 0
+
+    def test_commander_command(self):
+        cmder = Commander()
+        args = [["bar", "baz"], 12]
+        cmder.command("foo", *args)
+        cmd = Commander.commands[-1]
+
+        self.assertEqual(cmd["method"], "foo")
+        self.assertEqual(cmd["key"], cmder.key)
+        self.assertEqual(cmd["args"], args)
+
+    def test_commander_create(self):
+        old_count = Commander._objectCount
+
+        args = [1, 2, 3]
+        cmder = Commander(*args)
+        cmd = Commander.commands[-1]
+
+        self.assertEqual(old_count + 1, Commander._objectCount)
+        self.assertEqual(cmd["method"], cmder.__class__.__name__)
+        self.assertEqual(cmd["args"], args)
+
+    def test_commander_destroy(self):
+        old_count = Commander._objectCount
+
+        cmder = Commander()
+        cmder.destroy()
+        cmd = Commander.commands[-1]
+
+        self.assertEqual(old_count, Commander._objectCount)
+        self.assertEqual(cmd["method"], "destroy")
