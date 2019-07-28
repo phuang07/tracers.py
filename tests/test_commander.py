1c37e4b15419f154b03a095d37cf87c6b40ea3fa
MarkKoz <KozlovMark@gmail.com>
2019-07-27 18:13:02 -0700

    Refactor Commander tests
    * Reset object count and commands list to previous values
    * Use dictionaries for all assertions rather than checking single keys
    * Create a Commander object in setUp() for use with most tests

--- tests/test_commander.py
+++ tests/test_commander.py
@@ -6,53 +6,67 @@ from algorithm_visualizer.types import UNDEFINED
 
 
 class CommanderTests(unittest.TestCase):
+    def setUp(self):
+        self.commander = Commander()
+
+    def test_commander_create(self):
+        old_count = Commander._objectCount
+
+        args = [1, 2, 3]
+        cmder = Commander(*args)
+        cmd = Commander.commands[-1]
+
+        expected_cmd = {
+            "key": cmder.key,
+            "method": "Commander",
+            "args": args,
+        }
+
+        self.assertEqual(old_count + 1, Commander._objectCount)
+        self.assertEqual(cmd, expected_cmd)
+
     def test_commander_max_commands(self):
+        old_cmds = Commander.commands
         Commander.commands = [None for _ in range(commander._MAX_COMMANDS)]
 
         with self.assertRaisesRegex(RuntimeError, "Too Many Commands"):
-            Commander()
+            self.commander.command("foo")
 
-        Commander.commands = []
+        Commander.commands = old_cmds
 
     def test_commander_max_objects(self):
+        old_count = Commander._objectCount
         Commander._objectCount = 200
 
         with self.assertRaisesRegex(RuntimeError, "Too Many Objects"):
             Commander()
 
-        Commander._objectCount = 0
+        Commander._objectCount = old_count
 
     def test_commander_command(self):
-        cmder = Commander()
         args = [["bar", "baz"], 12]
-        cmder.command("foo", *args, UNDEFINED)
+        self.commander.command("foo", *args, UNDEFINED)
         cmd = Commander.commands[-1]
 
         expected_cmd = {
-            "key": cmder.key,
+            "key": self.commander.key,
             "method": "foo",
             "args": args,
         }
 
         self.assertEqual(cmd, expected_cmd)
 
-    def test_commander_create(self):
-        old_count = Commander._objectCount
-
-        args = [1, 2, 3]
-        Commander(*args)
-        cmd = Commander.commands[-1]
-
-        self.assertEqual(old_count + 1, Commander._objectCount)
-        self.assertEqual(cmd["method"], "Commander")
-        self.assertEqual(cmd["args"], args)
-
     def test_commander_destroy(self):
         old_count = Commander._objectCount
 
-        cmder = Commander()
-        cmder.destroy()
+        self.commander.destroy()
         cmd = Commander.commands[-1]
 
-        self.assertEqual(old_count, Commander._objectCount)
-        self.assertEqual(cmd["method"], "destroy")
+        expected_cmd = {
+            "key": self.commander.key,
+            "method": "destroy",
+            "args": [],
+        }
+
+        self.assertEqual(old_count - 1, Commander._objectCount)
+        self.assertEqual(cmd, expected_cmd)
