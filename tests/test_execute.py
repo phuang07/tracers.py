91723d7899b1848bd03a37a25aa1680f777f9c9c
MarkKoz <KozlovMark@gmail.com>
2019-08-27 10:49:31 -0700

    Add get_url tests

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -3,6 +3,7 @@ import unittest
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from unittest import mock
+from urllib.error import HTTPError
 
 import algorithm_visualizer
 
@@ -28,4 +29,18 @@ class TestExecute(unittest.TestCase):
                 actual_cmds = json.load(f)
 
         self.assertEqual(expected_cmds, actual_cmds)
+
+    def test_get_url(self):
+        url = algorithm_visualizer.get_url()
+        self.assertIsInstance(url, str)
+
+    @mock.patch.object(algorithm_visualizer, "urlopen")
+    def test_get_url_non_200(self, mock_urlopen):
+        response = mock.Mock()
+        type(response).status = mock.PropertyMock(return_value=201)
+        response.info = mock.Mock(return_value={})
+
+        mock_urlopen.return_value = response
+
+        self.assertRaises(HTTPError, algorithm_visualizer.get_url)
 
